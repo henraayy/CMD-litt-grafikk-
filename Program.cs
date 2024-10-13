49abf229aaf3b20e82fdad516a74b4a659ba3367
henraayy <144103145+henraayy@users.noreply.github.com>
2024-10-13 23:10:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace NTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = "*";
+            bool increasing = true;  //sjekker om det blir lagt til eller fjernes stjerner
+
+            while (true)  //uendelig loop
+            {
+                Console.WriteLine(symbol);
+                
+                if (increasing)
+                {
+                    symbol += "*";  //legger til en stjerne
+                    if (symbol.Length >= 10)
+                    {
+                        increasing = false;  //bytt til å fjerne stjerner når den når 10
+                    }
+                }
+                else
+                {
+                    symbol = symbol.Remove(symbol.Length - 1);  //fjerner en stjerne
+                    if (symbol.Length <= 1)
+                    {
+                        increasing = true;  //bytt til å legge til stjerner igjen når den er på 1
+                    }
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
